fbf0161063261cca9e518d1fab9c1a221dfd8913
samylegalloudec <samy.legalloudec@gmail.com>
2019-10-09 17:21:06 +0200

    premier dépot

---
+++ src/aeroport/internal/utilities/MqttUtilities.go
@@ -0,0 +1,32 @@
+package mqttUtilities
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func createClientOptions(brokerURI string, clientId string) *mqtt.ClientOptions {
+	opts := mqtt.NewClientOptions()
+
+	opts.AddBroker(brokerURI)
+
+	opts.SetClientID(clientId)
+	return opts
+}
+
+func connect(brokerURI string, clientId string) mqtt.Client {
+	fmt.Println("Trying to connect (" + brokerURI + ", " + clientId + ")...")
+	opts := createClientOptions(brokerURI, clientId)
+	client := mqtt.NewClient(opts)
+	token := client.Connect()
+	for !token.WaitTimeout(3 * time.Second) {
+
+	}
+	if err := token.Error(); err != nil {
+		log.Fatal(err)
+	}
+	return client
+}
